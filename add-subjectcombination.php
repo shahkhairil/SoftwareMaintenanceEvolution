a5c42f29882cd7adfba7ef05dfd607d5f4be5571
shahkhairil <58905847+shahkhairil@users.noreply.github.com>
2021-01-20 14:46:55 +0800

    add page

--- add-subjectcombination.php
+++ add-subjectcombination.php
@@ -4,6 +4,24 @@
 	</head>
 	
 	<body>
-		
+		<div class="main-page">
+			<div class="container-fluid">
+				<div class="row page-title-div">
+					<div class="col-md-6">
+						<h2 class="title">Add Subject Combination</h2>
+					</div>
+				</div>
+
+				<div class="row breadcrumb-div">
+                    <div class="col-md-6">
+                        <ul class="breadcrumb">
+                            <li><a href="dashboard.php"><i class="fa fa-home"></i> Home</a></li>
+                                <li> Subjects</li>
+                            <li class="active">Add Subject Combination</li>
+                        </ul>
+                    </div>
+                </div>
+			</div>
+		</div>
 	</body>
 </html>
