cbeda01594aa070e43d2cef5f098053d9e4b57b0
Shigihara1995 <evetsteven95@gmail.com>
2021-01-24 20:41:36 +0800

    Update edit-student.php

--- edit-student.php
+++ edit-student.php
@@ -14,16 +14,18 @@ if(isset($_POST['submit']))
 {
 $studentname=$_POST['fullanme'];
 $roolid=$_POST['rollid']; 
-$studentemail=$_POST['emailid']; 
+$studentemail=$_POST['emailid'];
+$telNo=$_POST['telno'];
 $gender=$_POST['gender']; 
 $classid=$_POST['class']; 
 $dob=$_POST['dob']; 
 $status=$_POST['status'];
-$sql="update tblstudents set StudentName=:studentname,RollId=:roolid,StudentEmail=:studentemail,Gender=:gender,DOB=:dob,Status=:status where StudentId=:stid ";
+$sql="update tblstudents set StudentName=:studentname,RollId=:roolid,StudentEmail=:studentemail,TelephoneNo=:telNo,Gender=:gender,DOB=:dob,Status=:status where StudentId=:stid ";
 $query = $dbh->prepare($sql);
 $query->bindParam(':studentname',$studentname,PDO::PARAM_STR);
 $query->bindParam(':roolid',$roolid,PDO::PARAM_STR);
 $query->bindParam(':studentemail',$studentemail,PDO::PARAM_STR);
+$query->bindParam(':telNo',$telNo,PDO::PARAM_STR);
 $query->bindParam(':gender',$gender,PDO::PARAM_STR);
 $query->bindParam(':dob',$dob,PDO::PARAM_STR);
 $query->bindParam(':status',$status,PDO::PARAM_STR);
